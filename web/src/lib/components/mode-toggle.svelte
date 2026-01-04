bee6ef657eecf57cd6cd0332a6c80bde498a4915
imbagila <roid.rabbani@gmail.com>
2026-01-04 09:53:32 +0700

    chore: add oxc linter and formatter

--- web/src/lib/components/mode-toggle.svelte
+++ web/src/lib/components/mode-toggle.svelte
@@ -1,11 +1,11 @@
 <script lang="ts">
     import SunIcon from "@lucide/svelte/icons/sun";
     import MoonIcon from "@lucide/svelte/icons/moon";
-    
+
     import { toggleMode } from "mode-watcher";
     import { Button } from "$lib/components/ui/button/index.js";
    </script>
-    
+
    <Button onclick={toggleMode} variant="outline" size="icon">
     <SunIcon
      class="h-[1.2rem] w-[1.2rem] scale-100 rotate-0 !transition-all dark:scale-0 dark:-rotate-90"
