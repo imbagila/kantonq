15f68a4f53103b0174dce0933f4a7b2f1f45e16a
imbagila <roid.rabbani@gmail.com>
2026-01-15 00:30:57 +0700

    feat(api): add phalcon stubs

---
+++ api/index.php
@@ -0,0 +1,17 @@
+<?php
+declare(strict_types=1);
+
+use Phalcon\Mvc\Micro;
+
+$app = new Micro();
+
+$app->get(
+    '/',
+    function () {
+        echo "<h1>Hello World!</h1>";
+    }
+);
+
+$app->handle(
+    $_SERVER["REQUEST_URI"]
+);
